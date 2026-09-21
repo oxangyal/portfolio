c57e1e04e478bc5758507f1a4ecb3ed10cdb3bb2
oxangyal <Oxana.mitch@gmail.com>
2026-09-21 11:07:42 -0400

    feat: update Hero component styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
                             variants={container(0)}
                             initial="hidden"
                             animate="visible"
-                            className="pb-16 text-7xl font-thin tracking-tight lg:mt-12 lg:text-7xl"
+                            className="pb-8 text-4xl sm:text-6xl lg:text-7xl font-thin tracking-tight lg:mt-12 text-center lg:text-left"
                         >
                             Oxana Michkasova
                         </motion.h1>
@@ -26,7 +26,7 @@ const Hero = () => {
                             variants={container(0.5)}
                             initial="hidden"
                             animate="visible"
-                            className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent"
+                            className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-2xl sm:text-3xl lg:text-4xl tracking-tight text-transparent text-center lg:text-left"
                         >
                             Full-Stack Developer & UX Designer
                         </motion.span>
